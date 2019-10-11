d1ef6b5da3c53254b8df7a2d952db157900e3cfa
Jiayi Tang <jiayitang10@gmail.com>
2019-10-10 22:15:05 -0600

    Started with models, views...

--- location/models.py
+++ location/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+class CheckIn(models.Model):
+    person = models.CharField(max_length=50)
+    location = models.ForeignKey(Place, on_delete=models.SET_NULL, blank=True, null=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} at {} at {}".format(self.person, self.location, self.time)
+
+    def check_in_is_fresh(self):
+        return self.time >= timezone.now() - datetime.timedelta(hours=2)
