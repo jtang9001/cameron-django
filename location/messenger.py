ff3f5a2e6c15585a79068d6603ef5623023b1aac
Jiayi Tang <jiayitang10@gmail.com>
2019-11-09 00:55:44 +0000

    handling places

--- location/messenger.py
+++ location/messenger.py
@@ -6,12 +6,13 @@ from .tokens import FB_ACCESS_TOKEN
 
 
 def cleanMsg(msg):
-    return ''.join(char.lower() for char in msg if char.isalnum() or char in ":")
+    return ''.join(char.lower() for char in msg if char.isalnum() or char in " :")
 
 class MessengerUser:
     def __init__(self, userID):
         self.userID = userID
-    
+        self.state = "init"
+
     def handleMessage(self, inMsg):
         msg = cleanMsg(inMsg)
         print(msg)
@@ -19,21 +20,39 @@ class MessengerUser:
         if "whos in" in msg:
             location = " ".join(msg.split()[2:])
             matchingPlaces = Place.objects.filter(name__icontains = location)
-            
+
             if len(matchingPlaces) == 0:
                 self.send("I don't know where you mean :(")
-                return
+
             elif len(matchingPlaces) > 1:
                 self.send("More than one matching place was found!")
-            
+
             for place in matchingPlaces:
-                self.send(f"Here's who's in {place.name}:")
+
                 checkins = place.checkin_set.all().order_by("end_time")
-                self.send(
-                    "\n".join(
-                        checkin.pretty() for checkin in checkins
+                freshCheckIns = (checkin for checkin in checkins if checkin.is_fresh())
+                futureCheckIns = (checkin for checkin in checkins if checkin.is_future_fresh())
+
+                if len(freshCheckIns) > 0:
+                    self.send(f"Here's who's in {place.name}:")
+
+                    self.send(
+                        "\n".join(
+                            checkin.pretty() for checkin in freshCheckIns
+                        )
                     )
-                )
+
+                if len(futureCheckIns) > 0:
+                    self.send(f"Here's who will be in {place.name}:")
+                    self.send(
+                        "\n".join(
+                            checkin.pretty() for checkin in futureCheckIns
+                        )
+                    )
+
+                elif len(freshCheckIns) == 0 and len(futureCheckIns) == 0:
+                    self.send(f"Nobody's checked into {place.name}. Would you like to?")
+                    self.state = "checking_in"
 
         else:
             self.send(f"You said, '{inMsg}'. I don't understand, sorry!")
@@ -43,12 +62,12 @@ class MessengerUser:
         response_msg = json.dumps(
             {
                 "messaging_type": msgType,
-                "recipient": {"id":self.userID}, 
+                "recipient": {"id":self.userID},
                 "message": {"text":outMsg}
             }
         )
         status = requests.post(
-            endpoint, 
+            endpoint,
             headers={"Content-Type": "application/json"},
             data=response_msg)
         print(status.json())
