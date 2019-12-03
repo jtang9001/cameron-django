bb9487f6d4f0a35065151e8a4962fe816dac579b
Jiayi Tang <jiayitang10@gmail.com>
2019-12-03 14:04:44 -0700

    added photo, reduced undo QR functionality

--- location/messenger.py
+++ location/messenger.py
@@ -228,14 +228,15 @@ def checkout(checkin, user, person, allowFuture = False):
             person.send(
                 f"{user} checked you out of {checkin.place} 💨",
                 quick_replies=QuickReplyArray([QuickReply(
-                    "Undo",
-                    payload=f"{person} in {checkin.place} until {checkin.prettyTime()}"
+                    f"I'm in {checkin.place}",
+                    payload=f"{person} in {checkin.place}"
                 )])
             )
         checkin.scratch()
         return QuickReplyArray([QuickReply(
             f"Undo for {person}",
-            payload=f"{person} in {checkin.place} {checkin.prettyTime()}"
+            payload=f"{person} in {checkin.place}",
+            img=person.facebook_photo
         )])
 
     elif allowFuture and checkin.is_future_fresh():
@@ -247,14 +248,15 @@ def checkout(checkin, user, person, allowFuture = False):
             person.send(
                 f"{user} deleted your upcoming check in at {checkin.prettyNoName()}.",
                 quick_replies=QuickReplyArray([QuickReply(
-                    "Undo",
-                    payload=f"{person} in {checkin.place} {checkin.prettyTime()}"
+                    f"I'm in {checkin.place}",
+                    payload=f"{person} in {checkin.place}"
                 )])
             )
         checkin.scratch()
         return QuickReplyArray([QuickReply(
             f"Undo for {person}",
-            payload=f"{person} in {checkin.place} {checkin.prettyTime()}"
+            payload=f"{person} in {checkin.place}",
+            img=person.facebook_photo
         )])
 
     else:
